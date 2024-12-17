69266cd8cd23bfdf539acc2265fd62d2f5f2767d
joranmulderij <66296620+joranmulderij@users.noreply.github.com>
2024-12-17 09:29:53 +0100

    Delete myxfoil.m

--- myxfoil.m
+++
@@ -1,8 +0,0 @@
-function [airfoil] = myxfoil(coords, simops, varargin)
-
-argcell = num2cell(varargin);
-[airfoil.data,airfoil.foil] = xfoil(coords,simops.alpha,simops.Re,simops.Mach,'oper iter 60','ppar N 181','oper xtr 0.1 0.1', argcell{:});
-% gdes flap 0.7 0 1
-
-end
-
